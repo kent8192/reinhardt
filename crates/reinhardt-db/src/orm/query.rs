9fe927542c54f33fd72a3936960f4e0ade19eb7b
kent8192 <51869472+kent8192@users.noreply.github.com>
2026-07-31 19:58:58 +0900

    style(db): format explain rows assertion

--- crates/reinhardt-db/src/orm/query.rs
+++ crates/reinhardt-db/src/orm/query.rs
@@ -10287,15 +10287,17 @@ mod tests {
 		);
 		assert_eq!(
 			output.body,
-			super::ExplainBody::Rows(vec![serde_json::Value::Object(serde_json::Map::from_iter([
-				("id".to_owned(), serde_json::Value::from(2)),
-				("parent".to_owned(), serde_json::Value::from(0)),
-				("notused".to_owned(), serde_json::Value::from(0)),
-				(
-					"detail".to_owned(),
-					serde_json::Value::String("SCAN test_users".to_owned()),
-				),
-			]))])
+			super::ExplainBody::Rows(vec![serde_json::Value::Object(serde_json::Map::from_iter(
+				[
+					("id".to_owned(), serde_json::Value::from(2)),
+					("parent".to_owned(), serde_json::Value::from(0)),
+					("notused".to_owned(), serde_json::Value::from(0)),
+					(
+						"detail".to_owned(),
+						serde_json::Value::String("SCAN test_users".to_owned()),
+					),
+				]
+			))])
 		);
 	}
 
